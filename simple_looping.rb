29dcbf6e25a107b7e979d90e08d2d297565742f8
clever-puppet-5071 <yoonmi.kim@rutgers.edu>
2019-03-12 21:07:56 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -33,7 +33,7 @@ def while_iterator(number_of_times)
   # hint: user a counter to tell the while loop when to stop!
   phrase = "Welcome to Flatiron School's Web Development Course!"
   count = 0
-  while count == number_of_times
+  while count < number_of_times
     puts phrase
     count += 1
   end

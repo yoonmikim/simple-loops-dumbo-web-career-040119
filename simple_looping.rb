04f7d6caed979acd2fd08e40fa5a8d452b846a3b
clever-puppet-5071 <yoonmi.kim@rutgers.edu>
2019-03-12 21:05:33 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -22,6 +22,9 @@ end
 def times_iterator(number_of_times)
   # code your solution here using the "times" keyword
   phrase = "Welcome to Flatiron School's Web Development Course!"
+  number_of_times.times do
+    puts phrase
+  end
   
 end
 
